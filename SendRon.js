ebfe3295f3bc60832b15a2ff42b7c8a18e9f71de
Dask <82965907+DaskSs@users.noreply.github.com>
2024-03-15 16:57:08 -0300

    Create SendRon.js

---
+++ SendRon.js
@@ -0,0 +1,37 @@
+const { ethers } = require("ethers");
+
+// Conectar ao provedor da Rede Ronin
+const provider = new ethers.providers.JsonRpcProvider('https://api.roninchain.com/rpc');
+
+// Substitua pelo método apropriado de segurança para suas chaves privadas
+const privateKey = '';
+const wallet = new ethers.Wallet(privateKey, provider);
+
+// Lista de endereços das carteiras destinatárias
+const addresses = [
+  '',
+  ''
+  // Adicione mais endereços conforme necessário
+];
+
+// Função para enviar tokens
+async function sendTokens() {
+  const transactionAmount = ethers.utils.parseUnits('0.02', 'ether'); // 0.2 RON, ajuste conforme a unidade correta
+
+  for (const address of addresses) {
+    const tx = {
+      to: address,
+      value: transactionAmount
+    };
+
+    try {
+      const txResponse = await wallet.sendTransaction(tx);
+      console.log(`Transação enviada! Hash: ${txResponse.hash}`);
+      // Aguarde a confirmação aqui, se necessário
+    } catch (error) {
+      console.error(`Erro ao enviar para ${address}: ${error}`);
+    }
+  }
+}
+
+sendTokens();
